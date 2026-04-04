972f123a2a58578789248d7d58b763d2f0fafa76
Patotatoman27 <A00839570@tec.mx>
2026-04-04 02:18:12 -0600

    Sidebar as main Component

--- client/src/core/router/AppRouter.tsx
+++ client/src/core/router/AppRouter.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/features/auth/pages/LogInPage';
 import ProfilePage from '@/features/profile/pages';
 import UserDashboardPage from '@/features/user/pages/DashboardPage';
+import HeaderView from "@/features/user/components/Header"
+import SidebarView from "@/features/user/components/Sidebar"
 
 export function AppRouter() {
   return (
@@ -10,6 +12,8 @@ export function AppRouter() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/user" element={<UserDashboardPage />}/>
+      <Route path="/header" element={<HeaderView />}/>
+      <Route path="/sidebar" element={<SidebarView />}/>
     </Routes>
   );
 }
