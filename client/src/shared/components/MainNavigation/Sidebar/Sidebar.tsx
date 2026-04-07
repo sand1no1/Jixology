27a89e5bcc77b0e9c453092808f80bd623850fb6
Patotatoman27 <A00839570@tec.mx>
2026-04-07 17:37:30 -0600

    Decoration of Header/Sidebar

--- client/src/shared/components/MainNavigation/Sidebar/Sidebar.tsx
+++ client/src/shared/components/MainNavigation/Sidebar/Sidebar.tsx
@@ -1,69 +1,50 @@
 import React from 'react';
 import type { ReactNode } from 'react';
-import Profile from '@/features/profile/pages';
 import './Sidebar.css';
-
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import { 
+  BeakerIcon,
+  UserCircleIcon,
+  LockClosedIcon
+} from '@heroicons/react/24/solid';
 
 export interface ISidebarProps {
   children?: ReactNode;
 }
 
-const Sidebar: React.FC<ISidebarProps> = ({ children }) => {
-  return (<>
+const Sidebar: React.FC<ISidebarProps> = () => {
+  return (
     <nav>
-      <div className="Header">
-        <div className="HeaderMain">
-          <img src="../../TechMahindraLogo.png"></img>
-          <span><b>Proyecto Principal</b></span>
+      <div className="header">
+        <div className="headerMain">
+          <img src="../../TechMahindraLogo.png" />
+          <span>Proyecto Principal</span>
         </div>
       </div>
-      
 
-      <div className="Sidebar">
-          <div className="SidebarHeader">
-            <span className="SidebarImage">
-              <img src="../../TechMahindraLogoSmall.png"></img>
-            </span>
-            <div className="SidebarMainTitle">
-              <span className="SidebarTitle"><b>Jixology Devsys</b></span>
-              <span className="SidebarSubtitle">Tech Mahindra's Development Environment</span>
-            </div>
-          </div>
-          <ul className="SidebarMenu">
-            <li className="SidebarMenuElement">
-              <a href="#">
-                <div className="SidebarMenuImage">
-                  <BeakerIcon style={{ width: '24px', height: '24px' }} />
-                  <span>Perfil</span>
-                </div>
-                
-              </a>
-            </li>
-            <li className="SidebarMenuElement">
-              <a href="#">
-                <div className="SidebarMenuImage">
-                  <BeakerIcon style={{ width: '24px', height: '24px' }} />
-                  <span>Perfil</span>
-                </div>
-                
-              </a>
-            </li>
-            <li className="SidebarMenuElement">
-              <a href="#">
-                <div className="SidebarMenuImage">
-                  <BeakerIcon style={{ width: '24px', height: '24px' }} />
-                  <span>Perfil</span>
-                </div>
-                
-              </a>
-            </li>
-          </ul>
-      </div>
-
-      
+      <aside className="sidebar">
+        <ul className="menu">
+          <li className="menuItem">
+            <a href="#">
+              <UserCircleIcon className="icon" />
+              <span><b>Perfil</b></span>
+            </a>
+          </li>
+          <li className="menuItem">
+            <a href="#">
+              <BeakerIcon className="icon" />
+              <span><b>Proyectos</b></span>
+            </a>
+          </li>
+          <li className="menuItem">
+            <a href="#">
+              <LockClosedIcon className="icon" />
+              <span><b>Verificación</b></span>
+            </a>
+          </li>
+        </ul>
+      </aside>
     </nav>
-  </>);
+  );
 };
 
 export default Sidebar;
