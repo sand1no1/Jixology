b1dc0c165572b17ea2be5b00d2074169e9222781
javierlcastillo <jazer2228@gmail.com>
2026-04-16 18:54:30 -0600

    lint fix

--- client/src/core/auth/userContext.tsx
+++ client/src/core/auth/userContext.tsx
@@ -41,6 +41,7 @@ export function UserProvider({ children }: {children: React.ReactNode}) {
     );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useUser(): UserContextValue {
     const context = useContext(UserContext);
     if (!context) throw new Error('useUser debe usarse dentro de UserProvider');
