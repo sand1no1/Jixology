26683766058f45eac2e2b528cfb3a0638997ed7c
javierlcastillo <jazer2228@gmail.com>
2026-04-16 21:01:01 -0600

    Lint fix

--- client/src/features/projects/pages/ProjectsPage/ProjectsPage.tsx
+++ client/src/features/projects/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,5 +1,4 @@
 import React, { useMemo, useState } from 'react';
-import { CodeBracketIcon } from '@heroicons/react/24/outline';
 import styles from './ProjectsPage.module.css';
 import { useUser } from '@/core/auth/userContext';  
 
