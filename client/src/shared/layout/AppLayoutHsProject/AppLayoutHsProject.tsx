4cc3b22535ac6dbfd583382d1294503977a61680
Patotatoman27 <A00839570@tec.mx>
2026-04-23 15:44:31 -0600

    Sidebar Modifications

--- client/src/shared/layout/AppLayoutHsProject/AppLayoutHsProject.tsx
+++ client/src/shared/layout/AppLayoutHsProject/AppLayoutHsProject.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import type { ReactNode } from 'react';
 import styles from './AppLayoutHsProject.module.css';
+// Imports de React Dom
 import {Outlet} from "react-router-dom"
+//import { useNavigate, useParams, Navigate } from 'react-router-dom';
+//import { useUser } from '@/core/auth/userContext';
+//Referencias
 import Header from '@/shared/layout/Header';
 import Sidebar from '@/shared/layout/Sidebar';
 import ProjectHeader from '@/shared/layout/HeaderProject';
